84fe34b556e497cb1190c8a8cebbb0f3ce6adf66
MichaelRod <cequea.mrgmail.com>
2022-03-15 18:40:59 -0400

    Creacion metodo delete y put

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,7 +1,10 @@
 const {Router} = require('express');
 const router = Router();
 
-const movies = require('./sample.json');
+//Es un paquete que permite manipular arreglos
+const _ = require('underscore');
+
+const movies = require('./sample.json');// Archivo .Json que emula DB  
 //console.log(movies);
 
 router.get('/',(req,res)=>{
@@ -9,22 +12,53 @@ router.get('/',(req,res)=>{
 });
 
 router.post('/',(req,res)=>{
-    const {title,director,year,rating} = req.body;//Se obtienen los datos
+    
+    const { title, director, year, rating }= req.body;//Se obtienen los datos
     
     if(title && director && year && rating) { //Se verifican que estan los datos
        
-        const id = movies.length + 1;
-        
-        const newMovie = {...req.body, id };   //Se guardan los datos
-        
-        console.log(newMovie);
-        
-        res.json('Data is Saved');
+        const id = movies.length + 1;// Incrementa el ID 
+        const newMovie = {id, ...req.body };   //Se guardan los datos
+        movies.push(newMovie);
+        res.json(movies)
     }else{
-        res.send('Error sent data is not correct');
+        res.status(500).json({"error":"There was an error."});
+    }
+        
+    
+});
+
+router.put('/:id',(req, res) => {
+    
+    const { id } = req.params;
+    const { title, director, year, rating }= req.body;
+   
+    if( title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if(movie.id == id) {
+                movie.title = director;
+                movie.director = title;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+       
+        res.json(movies);
+        
+    } else {
+        res.status(500).json({error: 'Datos incorrectos'});
     }
 });
 
+router.delete('/:id',(req,res)=>{
+    const {id} =req.params; // Valor que viene desde la solicitud 
+    _.each(movies,(movie,i)=>{
+       if(movie.id == id) {
+           movies.splice(i, 1);
+       } 
+    });
+    res.send(movies);
+});
 
 
 module.exports = router;
